1d8f8e24215465a2384fc29246d3603e7a9d59f5
Elias De Windt <edewindt27@outlook.com>
2023-06-04 00:50:41 -0700

    added left case for disconnected websockets

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -101,6 +101,12 @@ func ListenToWsChannel() {
 			response.Action = "list_users"
 			response.ConnectedUsers = users
 			broadcastToAll(response)
+		case "left":
+			response.Action = "list_users"
+			delete(clients, e.Conn)
+			users := getUserList()
+			response.ConnectedUsers = users
+			broadcastToAll(response)
 			//get a list of all users and send it back via broadcast
 		}
 
